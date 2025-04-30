9970980ed5666ac328d4ab73034323e44ae2b32c
gdenayer <denayer@hsu-hh.de>
2025-04-30 09:46:05 +0200

    Formatting.

--- Emperor/src/mapping/DataFieldIntegrationAdapter.cpp
+++ Emperor/src/mapping/DataFieldIntegrationAdapter.cpp
@@ -17,35 +17,35 @@
 namespace EMPIRE {
 
 DataFieldIntegrationAdapter::DataFieldIntegrationAdapter(AbstractMesh *_mesh) :
-	mesh(_mesh) {
-	if(mesh->type == EMPIRE_Mesh_FEMesh) {
-	    FEMesh *feMesh = dynamic_cast<FEMesh*>(_mesh);
-	    initDataFieldIntegrationMesh(feMesh);
-   } else if(mesh->type == EMPIRE_Mesh_IGAMesh) {
-	    IGAMesh *igaMesh = dynamic_cast<IGAMesh*>(_mesh);
-	    initDataFieldIntegrationNURBS(igaMesh);
-   } else
-		ERROR_BLOCK_OUT("DataFieldIntegrationAdapter","DataFieldIntegrationAdapter","DataFieldIntegration not implemented for mesh other than standard FE mesh or NURBS surfaces");
+        mesh(_mesh) {
+        if(mesh->type == EMPIRE_Mesh_FEMesh) {
+            FEMesh *feMesh = dynamic_cast<FEMesh*>(_mesh);
+            initDataFieldIntegrationMesh(feMesh);
+        } else if(mesh->type == EMPIRE_Mesh_IGAMesh) {
+            IGAMesh *igaMesh = dynamic_cast<IGAMesh*>(_mesh);
+            initDataFieldIntegrationNURBS(igaMesh);
+        } else
+            ERROR_BLOCK_OUT("DataFieldIntegrationAdapter","DataFieldIntegrationAdapter","DataFieldIntegration not implemented for mesh other than standard FE mesh or NURBS surfaces");
 }
 
 DataFieldIntegrationAdapter::~DataFieldIntegrationAdapter() {
-	delete dataFieldIntegrationImpl;
+        delete dataFieldIntegrationImpl;
 }
 
 void DataFieldIntegrationAdapter::initDataFieldIntegrationMesh(FEMesh *_mesh) {
-	dataFieldIntegrationImpl=new DataFieldIntegration(_mesh);
+        dataFieldIntegrationImpl=new DataFieldIntegration(_mesh);
 }
 
 void DataFieldIntegrationAdapter::initDataFieldIntegrationNURBS(IGAMesh *_mesh) {
-	dataFieldIntegrationImpl=new DataFieldIntegrationNURBS(_mesh);
+        dataFieldIntegrationImpl=new DataFieldIntegrationNURBS(_mesh);
 }
 
 void DataFieldIntegrationAdapter::integrate(const double *tractions, double *forces) {
-	dataFieldIntegrationImpl->integrate(tractions,forces);
+        dataFieldIntegrationImpl->integrate(tractions,forces);
 }
 
 void DataFieldIntegrationAdapter::deIntegrate(const double *forces, double *tractions) {
-	dataFieldIntegrationImpl->deIntegrate(forces, tractions);
+        dataFieldIntegrationImpl->deIntegrate(forces, tractions);
 }
 
 } /* namespace EMPIRE */
